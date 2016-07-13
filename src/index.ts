7fa3a29cabc1e6c48da66962be2ba255f0740e07
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-07-13 15:37:25 +0200

    fix(index): wrong boolean for CSS extraction

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ export = function css({ filename = '[name].css', allChunks = false, sourceMap = 
       sourceMap = true // source maps need to be on for this
     }
 
-    const extractCss = extractText === false
+    const extractCss = extractText !== false
     const providedInstance = extractText instanceof ExtractTextPlugin
     if (!providedInstance)
       extractText = extractCss ? new ExtractTextPlugin(filename, extractText instanceof Object ? extractText : { allChunks, sourceMap }) : null
